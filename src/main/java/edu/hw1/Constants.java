d5df2b63b01acb9210908d5df010612cab1d2fef
Яна <solodovnikovasyv@yandex.ru>
2023-10-14 23:21:25 +0300

    Done Task 1

---
+++ src/main/java/edu/hw1/Constants.java
@@ -0,0 +1,17 @@
+package edu.hw1;
+
+public class Constants {
+
+    private Constants() {
+    }
+
+    public static final int MAX_SECONDS_VALUE = 60;
+    public static final int BASIS_OF_NUMBER_SYSTEM_10 = 10;
+    public static final int BASIS_OF_NUMBER_SYSTEM_2 = 2;
+    public static final int KAPREKAR_CONST = 6174;
+    public static final int MIN_FOUR_DIGIT_NUMBER = 1000;
+    public static final int MIN_FIVE_DIGIT_NUMBER = 10000;
+    public static final int MAX_DIGIT = 9;
+    public static final int MIN_DIGIT = 0;
+    public static final int BOARD_SIZE = 8;
+}
